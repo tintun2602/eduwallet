e8e65b00441dc05b70fdc62620773c519f33f176
Diego Da Giau <diego.22@outlook.it>
2025-02-18 11:10:01 +0100

    feat: add MemoryRouter to the extension. Add new routes to App file. Set index.html file.

--- browser-extension/src/App.tsx
+++ browser-extension/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import LoginPage from './pages/LoginPageComponent';
+import Homepage from './pages/HomePageComponent';
+import { User } from './models/user';
+import Layout from './components/LayoutComponent';
+import UserPage from './pages/UserPageComponent';
+
+
+import { ntnu, polito, student, results } from './db';
+import University from './models/university';
+import { Outlet, Navigate } from 'react-router-dom';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [user, setUser] = useState<User>();
+  const [universities, setUniversities] = useState<University[]>([]);
+  //const [menuOption, setMenuOption] = useState<number>(1);
+
+  useEffect(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  useEffect(() => {
+    const userTemp: User = new User(student.id, student.name, student.surname, student.birthDate, student.birthPlace, student.country);
+    userTemp.setResults(results);
+    setUser(userTemp);
+    const unis: University[] = [
+      new University(polito.id, polito.name, polito.shortName),
+      new University(ntnu.id, ntnu.name, ntnu.shortName)
+    ];
+    setUniversities(unis.sort((a, b) => a.getName().localeCompare(b.getName())));
+  }, []);
+
+  const ProtectedRoute = () => {
+    return isLoggedIn && user ? <Layout><Outlet /></Layout> : <Navigate to="/login" />;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<ProtectedRoute />}>
+        <Route index element={user && <Homepage user={user} universities={universities} />} />
+      </Route>
+      <Route path='/user' element={<ProtectedRoute />}>
+        <Route index element={user && <UserPage user={user} />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App
