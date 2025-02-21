5b9c2f02347a8adc02b2ec11285640fc3b51d82d
Diego Da Giau <diego.22@outlook.it>
2025-02-21 21:28:36 +0100

    feat: add Typescript to Hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,24 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      accounts: {
+        count: 10
+      }
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
+};
+
+export default config;
