a4f6fe81218159e992ccaae4700f09b601aea330
Diego Da Giau <diego.22@outlook.it>
2025-02-23 12:17:17 +0100

    fix: fix usage of vite polyfills for chrome extension

--- browser-extension/vite.config.ts
+++ browser-extension/vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     nodePolyfills()
   ],
+  build: {
+    rollupOptions: {
+      external: ['vite-plugin-node-polyfills/shims/global'],
+      output: {
+        globals: {
+          'vite-plugin-node-polyfills/shims/global': 'global'
+        }
+      }
+    }
+  }
 })
