0d7524b89c919eb07079ff7c6a2042f6b8bb1a96
Diego Da Giau <diego.22@outlook.it>
2025-02-24 20:16:26 +0100

    chore: delete unused dependencies

--- browser-extension/vite.config.ts
+++ browser-extension/vite.config.ts
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [
-    react(),
-    nodePolyfills()
-  ],
-  build: {
-    rollupOptions: {
-      external: ['vite-plugin-node-polyfills/shims/global'],
-      output: {
-        globals: {
-          'vite-plugin-node-polyfills/shims/global': 'global'
-        }
-      }
-    }
-  }
+    react()
+  ]
 })
